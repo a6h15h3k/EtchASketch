c1509858f7ec9390d0fb57dc59b92e05c1e000e8
a6hi <saraffabhishek@outlook.com>
2022-08-04 21:31:43 -0400

    Javascript Boiler Plate

---
+++ script.js
@@ -0,0 +1,15 @@
+//Changes Grid Size as per user input
+function setResolution(pixels)    {
+
+}
+
+//Resets Grid to Default Color
+function reset()    {
+
+}
+
+//Grid divs change color when your mouse passes over them, 
+//leaving a (pixelated) trail through your grid like a pen would
+function hover()    {
+
+}
