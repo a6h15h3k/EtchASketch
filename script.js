1e9feea6ec2a764b21bb5e2ba400733e70fbf32a
a6hi <saraffabhishek@outlook.com>
2022-08-05 21:05:35 -0400

    Added Event Listeners and Hover Effect

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
+const body = document.querySelector("body");
+let canvas = document.querySelector("#canvas");
 const resetButton = document.querySelector("#reset");
 const resolutionButton = document.querySelector("#resolution");
-let canvas = document.querySelector("#canvas");
-const body = document.querySelector("body");
+const defaultResolution = 3;
 
-let tempDiv2 = document.createElement("div");
-let tempDiv3 = document.createElement("div");
-let tempDiv1 = document.createElement("div");
-
-tempDiv1.textContent = "hi";
-
-canvas.appendChild(tempDiv1);
+let resolution = defaultResolution;
+setResolution(resolution);
 
 resetButton.addEventListener('click', () => {
     reset();
+    setResolution(resolution);
 });
 
 resolutionButton.addEventListener('click', () => {
@@ -24,23 +21,26 @@ resolutionButton.addEventListener('click', () => {
 });
 
 //Changes Grid Size as per user input
-function setResolution(pixels) {
-    console.log(pixels);
+function setResolution(input) {
+    reset();
+    resolution = input;
+    for (let i = 1; i <= resolution; i++) {
+        canvas.appendChild(createRow(resolution));
+    }
 }
 
 //Prompts User to input a Resolution between 1 and 100
 function getResolution() {
-    let pixels = 0;
-    pixels = prompt("Please enter a pixel size between 1 and 100", Math.floor(Math.random() * 100 + 1));
-    return pixels;
+    let input = 0;
+    input = prompt("Please enter a pixel size between 1 and 100", Math.floor(Math.random() * 100 + 1));
+    return input;
 }
 
 //Resets Grid to Default Color
 function reset() {
     //Removes div Canvas, Thus clearing Etch-A-Sketch
-    console.log("reset");
     body.removeChild(canvas);
-    
+
     //Adds a new Empty Canvas
     let tempDiv = document.createElement("div");
     tempDiv.setAttribute("id", "canvas");
@@ -50,15 +50,49 @@ function reset() {
 
 //Grid divs change color when your mouse passes over them, 
 //leaving a (pixelated) trail through your grid like a pen would
-function hover() {
+function hover(pixel) {
+    let defaultColor = "white";
+    if(pixel.classList.contains("isBlack")) {
+        defaultColor = "black";
+    }
+    pixel.style.backgroundColor = "grey";
+    setTimeout(() => {
+        pixel.style.backgroundColor = defaultColor;
+    },200);    
 }
 
-/*
-let tempDiv2 = document.createElement("div");
-let tempDiv3 = document.createElement("div");
-let tempDiv1 = document.createElement("div");
+//Creates a Row Container, sets it's attributes and adds Pixels to it
+function createRow() {
+    let row = document.createElement("div");
+    row.style.flex = "1";
+    row.style.border = "2px solid green";
+    row.style.flexDirection = "row";
 
-tempDiv1.textContent = "hi";
+    for (i = 1; i <= resolution; i++) {
+        row.appendChild(createPixel());
+    }
 
-canvas.appendChild(tempDiv1);
-*/
+    return row;
+}
+
+//Creates a Pixel Square and sets it's attributes
+function createPixel() {
+    let pixel = document.createElement("div");
+    pixel.setAttribute("id", "pixel");
+    pixel.style.flex = "1";
+    pixel.textContent = "Yo";
+    pixel.style.border = "2px solid red";
+
+    pixel.addEventListener('click', () => {
+        pixel.style.backgroundColor = "black";
+        if(!pixel.classList.contains("isBlack")) {
+            pixel.classList.add("isBlack");
+        }
+    });
+
+    pixel.addEventListener('mouseover', () => {
+        hover(pixel);
+    });
+
+    return pixel;
+}
